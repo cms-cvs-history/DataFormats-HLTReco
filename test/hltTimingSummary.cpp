23284418fe41a400b82a3d64b4e5ddd36f7f7b97
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-01-09 08:42:21 +0000

    *** empty log message ***

--- test/hltTimingSummary.cpp
+++ test/hltTimingSummary.cpp
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <math.h>
+#include <cmath>
 
 #include "DataFormats/Provenance/interface/EventAuxiliary.h"
 #include "DataFormats/HLTReco/interface/HLTPerformanceInfo.h"
